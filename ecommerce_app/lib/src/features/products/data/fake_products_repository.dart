e16ae615a8b3d783c48209b125a943f791dbd17c
adriane purdy <adriane.purdy@slalom.com>
2024-03-25 17:33:25 -0700

    feat/branch2: adding flutter_riverpod (part1)

--- ecommerce_app/lib/src/features/products/data/fake_products_repository.dart
+++ ecommerce_app/lib/src/features/products/data/fake_products_repository.dart
@@ -1,9 +1,13 @@
+import 'dart:async';
+
 import 'package:ecommerce_app/src/constants/test_products.dart';
 import 'package:ecommerce_app/src/features/products/domain/product.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class FakeProductsRepository {
-  FakeProductsRepository._();
-  static FakeProductsRepository instance = FakeProductsRepository._();
+  // FakeProductsRepository._();
+  // static FakeProductsRepository instance = FakeProductsRepository._();
 
   final List<Product> _products = kTestProducts;
 
@@ -14,6 +18,13 @@ class FakeProductsRepository {
   Product? getProduct(String id) {
     return _products.firstWhere((product) => product.id == id);
   }
+
+// to test, can add in async:
+  // Future<List<Product>> fetchProductsList() async {
+  //   await Future.delayed(const Duration(seconds: 2));
+  //   throw Exception('connection failed');
+  //   return Future.value(_products);
+  // }
 
   Future<List<Product>> fetchProductsList() {
     return Future.value(_products);
@@ -28,3 +39,36 @@ class FakeProductsRepository {
         .map((products) => products.firstWhere((product) => product.id == id));
   }
 }
+
+final productsRepoProvider = Provider<FakeProductsRepository>((ref) {
+  // return FakeProductsRepository.instance;
+  return FakeProductsRepository();
+});
+
+final productsListStreamProvider =
+    StreamProvider.autoDispose<List<Product>>((ref) {
+  debugPrint('FakeProductsRepository, productsliststreamprovider');
+  final productsRepository = ref.watch(productsRepoProvider);
+  return productsRepository.watchProductsList();
+});
+
+final productsListFutureProvider =
+    FutureProvider.autoDispose<List<Product>>((ref) {
+  final productsRepository = ref.watch(productsRepoProvider);
+  return productsRepository.fetchProductsList();
+});
+
+final productProvider =
+    StreamProvider.autoDispose.family<Product?, String>((ref, id) {
+  debugPrint('FakeProductsRepository, productsliststreamprovider, id: $id');
+  ref.onDispose(() {
+    debugPrint(
+        'FakeProductsRepository, productsliststreamprovider, id: $id, disposed');
+  });
+  final link = ref.keepAlive();
+  Timer(const Duration(seconds: 10), () {
+    link.close();
+  });
+  final productsRepository = ref.watch(productsRepoProvider);
+  return productsRepository.watchProduct(id);
+});
