9be21c22e4048673cf56989853c334961e7cef86
adriane purdy <adriane.purdy@slalom.com>
2024-03-26 12:43:11 -0700

    feat/branch2: create fake auth repo

---
+++ ecommerce_app/lib/src/features/products/data/fake_auth_repository.dart
@@ -0,0 +1,52 @@
+import 'package:ecommerce_app/src/features/authentication/domain/app_user.dart';
+import 'package:ecommerce_app/src/utils/in_memory_store.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class FakeAuthRepo {
+  final _authState = InMemoryStore<AppUser?>(null);
+
+  Stream<AppUser?> authStateChanges() => _authState.stream;
+  AppUser? get currentUser => _authState.value;
+
+  // Stream<AppUser?> authStateChanges() => Stream.value(null);
+  // AppUser? get currentUser => null;
+
+  Future<void> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    if (currentUser == null) {
+      _createNewUser(email);
+    }
+  }
+
+  Future<void> createUserWithEmailAndPassword(
+      String email, String password) async {
+    if (currentUser == null) {
+      _createNewUser(email);
+    }
+  }
+
+  Future<void> signOut() async {
+    _authState.value = null;
+  }
+
+  void _createNewUser(String email) {
+    // note: the uid could be any unique string. Here we simply reverse the email.
+    _authState.value =
+        AppUser(uid: email.split('').reversed.join(), email: email);
+  }
+
+  void dispose() => _authState.close();
+}
+
+final authRepoProvider = Provider<FakeAuthRepo>((ref) {
+  final auth = FakeAuthRepo();
+  ref.onDispose(() => auth.dispose());
+  return auth;
+});
+
+final authStateChangeProvider = StreamProvider<AppUser?>((ref) {
+  final authRepo = ref.watch(authRepoProvider);
+  return authRepo.authStateChanges();
+});
